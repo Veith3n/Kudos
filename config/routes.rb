1b93fa603bd84645b1baa4019032675f0f1684c8
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-07-30 17:43:36 +0200

    add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'landing_page/index'
+
+  root 'landing_page#index'
 end
