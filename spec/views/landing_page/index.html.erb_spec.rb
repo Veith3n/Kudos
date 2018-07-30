8d3377b7752a9c5669b20a26fdbe8c4a81502873
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-07-30 17:43:22 +0200

    add tests for visiting landing page

---
+++ spec/views/landing_page/index.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'landing_page/index.html.erb', type: :view do
+  it 'have login and registe button' do
+
+    have_selector(:link_or_button, 'Login')
+    have_selector(:link_or_button, 'Register')
+  end
+end
