de74eae4d793ac1f5fd84859a87c6a651222a348
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-07-31 11:31:04 +0200

    update seeds.rb for Users and Teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+User.destroy_all
+Team.destroy_all
+
+40.times do
+  User.create(name: FFaker::Name.first_name, surname: FFaker::Name.last_name, email: FFaker::Internet.unique.email, password: 'test123#W', password_confirmation: 'test123#W', kudos_count: 0)
+  print '.'
+end
+
+puts ' '
+puts 'Users generated'
+
+8.times do
+  Team.create(name: FFaker::Company.name, description: FFaker::Company.bs)
+  Team.last.users << User.order("RANDOM()").limit(10)
+  print '.'
+end
+
+2.times do
+  Team.create(name: FFaker::Company.name, description: FFaker::Company.bs)
+  print '.'
+end
+
+puts ' '
+puts 'Teams generated'
+
+puts ' '
+puts 'Data generated'
