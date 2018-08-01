2d2fd21960f7b8126781b8368b22b0cff249495b
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-01 14:03:27 +0200

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    get 'register', to: 'devise/registrations#new', as: :new_user_registration
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  devise_for :users, skip: [:sessions]
 
   resources :users, only: [:index]
 
