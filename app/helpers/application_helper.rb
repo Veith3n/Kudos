65f2b74dbcb7a0982122b4cfc6cc0d5761b33494
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-02 12:21:31 +0200

    refactor code of application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,28 +1,24 @@
 module ApplicationHelper
   def flash_class(level)
-    case level
-    when :notice then
-      'alert alert-info'
-    when :success then
-      'alert alert-success'
-    when :error then
-      'alert alert-error'
-    when :alert then
-      'alert alert-error'
-    end
+    classes = {
+      notice: 'alert alert-info',
+      success: 'alert alert-success',
+      error: 'alert alert-error',
+      alert: 'alert alert-error'
+    }
+    classes[level]
   end
 
   def custom_bootstrap_flash
     flash.map do |type, message|
       type = 'success' if type == 'notice'
-      type = 'error'   if type == 'alert'
+      type = 'error' if type == 'alert'
       text = "<script>toastr.#{type}('#{message}');</script>"
       text.html_safe if message
     end.join("\n").html_safe
   end
 
   def current_class?(path)
-    return 'active' if request.path == path
-    ''
+    request.path == path ? 'active' : ''
   end
 end
