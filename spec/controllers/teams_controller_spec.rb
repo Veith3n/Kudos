57d6e3ec1cba642facd3d9232975649de75c4c2c
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-07 14:27:45 +0200

    add test for creating new team

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -45,4 +45,51 @@ RSpec.describe TeamsController, type: :controller do
       expect(team.users.count).to eq(0)
     end
   end
+
+  context '#new' do
+    it "won't render page when unsigned" do
+      get :new
+
+      expect(response).to have_http_status(302)
+    end
+
+    it 'returns HTTP success' do
+      sign_in(user, scope: :user)
+
+      get :new
+
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  context '#create' do
+    it "won't create team when unsigned" do
+      params = { name: 'Test', description: 'something we stand for' }
+
+      post :create, params: { team: params }
+
+      expect(response).to have_http_status(302)
+    end
+
+    it 'create team with valid params' do
+      sign_in(user, scope: :user)
+
+      params = { name: 'Test', description: 'something we stand for' }
+
+      post :create, params: { team: params }
+
+      expect(Team.last.name).to eq(params[:name])
+      expect(Team.last.description).to eq(params[:description])
+      expect(Team.count).to eq(1)
+    end
+
+    it 'does not create team with invalid params' do
+      sign_in(user, scope: :user)
+
+      params = { name: 'Test' }
+
+      expect { post :create, params: { team: params } }.to_not change { Team.count }
+      expect(subject).to render_template(:new)
+    end
+  end
 end
