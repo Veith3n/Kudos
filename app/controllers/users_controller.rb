74af542ee7625d58469371e1b880a0bf6e5a3678
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-07 18:00:14 +0200

    add implementation of giving kudo when user is at weekly limit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,16 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
 
     if (current_user.teams & user.teams).present? && current_user.id != user.id
-      if current_user.given_kudos.pluck(:receiver_id).include?(user.id)
-        redirect_to request.referrer, alert: 'You already gave a kudo to this user'
+      if current_user.kudos_given_in_a_week < current_user.weekly_kudos_limit
+        if current_user.given_kudos.pluck(:receiver_id).include?(user.id)
+          redirect_to request.referrer, alert: 'You already gave a kudo to this user'
+        else
+          current_user.increment!(:kudos_given_in_a_week)
+          Kudo.create(giver_id: current_user.id, receiver_id: user.id)
+          redirect_to request.referrer, notice: 'You gave a kudo'
+        end
       else
-        Kudo.create(giver_id: current_user.id, receiver_id: user.id)
-        redirect_to request.referrer, notice: 'You gave a kudo'
+        redirect_to request.referrer, alert: "You can't give more kudos this week"
       end
     else
       redirect_to request.referrer, alert: 'You are not team members'
