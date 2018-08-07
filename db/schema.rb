e85ba0ce7703bec6abc7528bdf0312d88c7c21fd
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-07 17:55:04 +0200

    update User model to have weekly_kudos_limit and kudos_given_in_a_week

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_103828) do
+ActiveRecord::Schema.define(version: 2018_08_07_152221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2018_08_07_103828) do
     t.string "provider"
     t.string "uid"
     t.boolean "skip_validation", default: false
+    t.integer "weekly_kudos_limit", default: 7
+    t.integer "kudos_given_in_a_week", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
