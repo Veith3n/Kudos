7e73bf16514b6f0475e5f96c75fd19aeb4375646
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-14 12:42:39 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_103828) do
+ActiveRecord::Schema.define(version: 2018_08_10_112605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,9 +58,9 @@ ActiveRecord::Schema.define(version: 2018_08_07_103828) do
     t.string "provider"
     t.string "uid"
     t.boolean "skip_validation", default: false
-    t.string "avatar"
     t.integer "weekly_kudos_limit", default: 7
     t.integer "kudos_given_in_a_week", default: 0
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
